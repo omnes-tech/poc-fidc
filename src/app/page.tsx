dbe5d1aefd813c5ee299c8ed0bf3261f1677bad1
Renan Cardoso <renancardoso0307@gmail.com>
2025-05-08 16:32:26 -0300

    feat: ultimos ajustes de limit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -77,7 +77,7 @@ export default function ManagerPage() {
     if(contractFidcId){
       getReceivables();
     }
-  },[contractFidcId]);
+  },[contractFidcId, currentEvents, currentOperation]);
 
   const handleInitializeFIDC = async () => {
     try {
